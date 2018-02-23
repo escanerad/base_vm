28900674909c3eead591a66845cb5f133cd85a26
albertpons <a.pons@escaner.ad>
2018-02-23 10:32:19 +0100

    Afegides fonts

--- scripts/03_prev_odoo.sh
+++ scripts/03_prev_odoo.sh
@@ -4,10 +4,18 @@
     sudo /opt/bitnami/ctlscript.sh restart apache
 
     #WKHTMLTOPDF
-    wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
-    tar -xvf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
-    sudo cp ./wkhtmltox/bin/wkhtmltoimage /usr/bin/
-    sudo cp ./wkhtmltox/bin/wkhtmltopdf /usr/bin/
+    #Instalar dependencies prèvies
+    #sudo apt-get install wkhtmltopdf
+    #Descarregar versió funcional
+    #wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+    #tar -xvf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+    #sudo cp ./wkhtmltox/bin/wkhtmltoimage /usr/bin/
+    #sudo cp ./wkhtmltox/bin/wkhtmltopdf /usr/bin/
     sudo /opt/bitnami/ctlscript.sh restart
+
+    #Instalar font BreuerText
+    sudo mkdir /usr/share/fonts/truetype/breuertext
+    sudo cp /home/bitnami/base_VM/fonts/Breuer* /usr/share/fonts/truetype/breuertext
+    echo 'FONTS INSTALADES'
 
 #FI ACCIONS VM ODOO
