469fff2a6c1fd1e2184b2f2ca805748f38d9b9f5
albertpons <a.pons@escaner.ad>
2018-03-06 00:59:51 +0100

    canvis

--- scripts/03_prev_odoo.sh
+++ scripts/03_prev_odoo.sh
@@ -15,7 +15,13 @@
 
     #Instalar font BreuerText
     sudo mkdir /usr/share/fonts/truetype/breuertext
-    sudo cp /home/bitnami/base_VM/fonts/Breuer* /usr/share/fonts/truetype/breuertext
-    echo 'FONTS INSTALADES'
+    sudo cp /home/bitnami/base_VM/fonts/BreuerText/*.ttf /usr/share/fonts/truetype/breuertext/
 
+    #Treure banner de la consola
+    sudo mv /opt/bitnami/var/data /opt/bitnami/var/data_original
+    sudo mv /opt/bitnami/scripts/init/welcome_message /opt/bitnami/scripts/init/welcome_message_original
+    sudo mv /opt/bitnami/scripts/init/welcome_message_reload /opt/bitnami/scripts/init/welcome_message__reload_original
+    sudo mv /opt/bitnami/scripts/init/update_welcome_file /opt/bitnami/scripts/init/update_welcome_file_original
+
+    
 #FI ACCIONS VM ODOO
