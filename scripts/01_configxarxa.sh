ffabc39c71e684ce3199c6042a81b8a02b009391
albertpons <a.pons@escaner.ad>
2018-03-05 15:22:34 +0100

    canvis automatitzacios

--- scripts/01_configxarxa.sh
+++ scripts/01_configxarxa.sh
@@ -3,18 +3,16 @@
     iface0=$(ip link show|grep ^[0-9]| grep -v lo|cut -f2 -d":"|sed 's/^[ \t]*//')
     echo Interface: $iface0
     echo ========================
-   # read -p 'Adreça IP(XXX.XXX.XXX.XXX/XX) :' ipaddress
-   # read -p 'Porta d´enllaç (Gateway) :' gateway
-   # read -p 'Servidor de Noms (DNS) :' DNS
-    
-    #sudo touch /etc/systemd/network/25-wired.network
-    #echo [MATCH] >> /etc/systemd/network/25-wired.network
-    #echo Name=$iface0  >> /etc/systemd/network/25-wired.network
-    #echo  >> /etc/systemd/network/25-wired.network
-    #echo [Network]  >> /etc/systemd/network/25-wired.network
-    #echo Address=$ipaddress  >> /etc/systemd/network/25-wired.network
-    #echo Gateway=$gateway  >> /etc/systemd/network/25-wired.network
-    #echo DNS=$DNS  >> /etc/systemd/network/25-wired.network
+    read -p 'Adreça IP(XXX.XXX.XXX.XXX/XX) :' ipaddress
+    read -p 'Porta d´enllaç (Gateway) :' gateway
+    read -p 'Servidor de Noms (DNS) :' DNS
+
+    sudo cp /etc/systemd/network/99-dhcp.network /etc/systemd/network/25-wired.network
+    sudo sed -i 's/Name=e\*/Name='$iface0'/g' /etc/systemd/network/25-wired.network
+    sudo sed -i '\/DHCP/d' /etc/systemd/network/25-wired.network
+    sudo sed -i '\/\[Network\]/a\Address\='$ipaddress /etc/systemd/network/25-wired.network
+    sudo sed -i '\/Address/a\Gateway\='$gateway /etc/systemd/network/25-wired.network
+    sudo sed -i '\/\Gateway/a\DNS\='$DNS /etc/systemd/network/25-wired.network
 
     #HOSTNAME
     read -p 'Nom complet del servidor (amb domini): ' servidor
--- scripts/02_SSL.sh
+++ scripts/02_SSL.sh
@@ -30,12 +30,19 @@
     sudo /opt/bitnami/ctlscript.sh start apache
 
     #FORÇAR REDIRECCIÓ A HTTPS
-    sudo vi /opt/bitnami/apache2/conf/bitnami/bitnami.conf
+    #sudo vi /opt/bitnami/apache2/conf/bitnami/bitnami.conf
+
     # A LA SECCIÓ <VirtualHost _default_:80> AFEGIR
     # RewriteEngine On
     # RewriteCond %{HTTPS} !=on
     # RewriteRule ^/(.*) https://%{SERVER_NAME}/$1 [R,L]
-
+    sudo cp /opt/bitnami/apache2/conf/bitnami/bitnami.conf /opt/bitnami/apache2/conf/bitnami/bitnami.conf.bak
+    sudo sed -i '/\DocumentRoot \"\/opt\/bitnami\/apache2\/htdocs\"/a\  RewriteEngine On' /opt/bitnami/apache2/conf/bitnami/bitnami.conf
+    # Eliminar el RewriteEngine de la secció SSL 
+    sudo vi /opt/bitnami/apache2/conf/bitnami/bitnami.conf
+    sudo sed -i '/\RewriteEngine On/a\  RewriteCond \%\{HTTPS\} \!\=on' /opt/bitnami/apache2/conf/bitnami/bitnami.conf
+    sudo sed -i '/\RewriteCond \%{HTTPS} \!\=on/a\  RewriteRule \^\/\(\.\*\) https\:\/\/\%{SERVER\_NAME}\$1 \[R\,L\]' /opt/bitnami/apache2/conf/bitnami/bitnami.conf
+    
     #RESTART APACHE SERVICE
     sudo /opt/bitnami/ctlscript.sh restart apache
 #FI CONFIGURACIÓ SSL
