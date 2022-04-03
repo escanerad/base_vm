cdcfaba5e34034edca1fa5777c1de7d2c3d8ec35
albertpons <a.pons@escaner.ad>
2022-04-03 18:52:26 +0200

    Update 05_git_clone.sh

--- scripts/05_git_clone.sh
+++ scripts/05_git_clone.sh
@@ -3,6 +3,14 @@ sudo mkdir /home/bitnami/addons
 sudo mkdir /home/bitnami/addons/prod
 sudo mkdir /home/bitnami/escaner/addons/repo
 cd /home/bitnami/escaner/addons/repo
+#PRE-REQUISITS
+sudo git clone -b 14.0 https://github.com/OCA/community-data-files
+sudo cp -r community-data-files/base_bank_from_iban ../prod
+sudo git clone -b 14.0 https://github.com/OCA/account-financial-tools
+sudo cp -r account-financial-tools/account_asset_management ../prod
+sudo git clone -b 14.0 https://github.com/OCA/reporting-engine
+sudo cp -r reporting-engine/report_xlsx ../prod
+sudo cp -r reporting-engine/report_xlsx_helper ../prod
 #LOCALITZACIÓ ESPANYOLA
 sudo git clone -b 14.0 https://github.com/OCA/l10n-spain
 sudo cp -r l10n-spain/l10n_es_partner* ../prod
@@ -12,16 +20,9 @@ sudo cp -r l10n-spain/l10n_es_account_* ../prod
 sudo git clone -b 14.0 https://github.com/OCA/contract
 sudo cp -r contract/contract ../prod/
 
-#DEPENDENCIES DEL MODUL ANTERIOR
-#    sudo git clone -b 10.0 https://github.com/OCA/account-analytic
-#    sudo git clone -b 10.0 https://github.com/OCA/account-financial-tools
-#    sudo git clone -b 10.0 https://github.com/OCA/bank-payment
-#    sudo git clone -b 10.0 https://github.com/OCA/sale-workflow
-#    sudo git clone -b 10.0 https://github.com/OCA/web
 
 #TRASPAS AL DIRECTORI D'ADDONS
-#sudo mkdir /opt/bitnami/apps/escaner
-#sudo mkdir /opt/bitnami/apps/escaner/addons
+sudo cp -r /home/bitnami/escanerad/addons/prod/* /bitnami/odoo/addons
 
 #modificar odoo-server.conf perque apunti al directori /opt/bitnami/apps/escaner/addons
 #sudo sed -i 's/addons_path/smtp_from \= "'$correu'"/g' /opt/bitnami/apps/odoo/conf/odoo-server.conf
