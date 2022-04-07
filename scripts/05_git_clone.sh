14f2e1def1aca6c83b1de51de53de2fc3bc34842
albertpons <a.pons@escaner.ad>
2022-04-07 14:03:31 +0200

    Update 05_git_clone.sh

--- scripts/05_git_clone.sh
+++ scripts/05_git_clone.sh
@@ -9,19 +9,32 @@ sudo cp -r community-data-files/base_bank_from_iban ../prod
 sudo git clone -b 14.0 https://github.com/OCA/account-financial-tools
 sudo cp -r account-financial-tools/account_asset_management ../prod
 sudo git clone -b 14.0 https://github.com/OCA/account-invoice-reporting
-sudo cp -r account-invoice-reporting/account_report_due_list ../prod
+sudo cp -r account-invoice-reporting/account_invoice_report_due_list ../prod
+sudo git clone -b 14.0 https://github.com/OCA/account-invoicing
+sudo cp -r account-invoicing/account_invoice_tax_note ../prod
+sudo cp -r account-invoicing/account_invoice_tax_required ../prod
 sudo git clone -b 14.0 https://github.com/OCA/reporting-engine
 sudo cp -r reporting-engine/report_xlsx ../prod
 sudo cp -r reporting-engine/report_xlsx_helper ../prod
+sudo git clone -b 14.0 https://github.com/OCA/mis-builder
+sudo cp -r mis-builder/mis_builder .. /prod
+
 
 #LOCALITZACIÓ ESPANYOLA
 sudo git clone -b 14.0 https://github.com/OCA/l10n-spain
 sudo cp -r l10n-spain/l10n_es_partner* ../prod
 sudo cp -r l10n-spain/l10n_es_account_* ../prod
+sudo cp -r l10n-spain/l10n_es_mis_report ../prod
+sudo cp -r l10n-spain/l10n_es_aeat ../prod
+sudo cp -r l10n-spain/l10n_es_aeat_mod303 ../prod
+sudo cp -r l10n-spain/l10n_es_aeat_mod390 ../prod
 
 #MÒDUL DE CONTRACTES
 sudo git clone -b 14.0 https://github.com/OCA/contract
 sudo cp -r contract/contract ../prod/
+sudo cp -r contract/contract_payment_mode ../prod/
+sudo cp -r contract/agreement_legal ../prod/
+
 
 
 #TRASPAS AL DIRECTORI D'ADDONS
