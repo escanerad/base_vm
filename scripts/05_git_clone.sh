ba41264fa4075195fc74ab4ba547c96aa68c3ebf
albertpons <a.pons@escaner.ad>
2024-01-16 12:48:58 +0100

    Update 05_git_clone.sh

--- scripts/05_git_clone.sh
+++ scripts/05_git_clone.sh
@@ -4,24 +4,24 @@ sudo mkdir /home/bitnami/escanerad/addons/prod
 sudo mkdir /home/bitnami/escanerad/addons/repo
 cd /home/bitnami/escanerad/addons/repo
 #PRE-REQUISITS
-sudo git clone -b 14.0 https://github.com/OCA/community-data-files
+sudo git clone -b 16.0 https://github.com/OCA/community-data-files
 sudo cp -r community-data-files/base_bank_from_iban ../prod
-sudo git clone -b 14.0 https://github.com/OCA/account-financial-tools
+sudo git clone -b 16.0 https://github.com/OCA/account-financial-tools
 sudo cp -r account-financial-tools/account_asset_management ../prod
-sudo git clone -b 14.0 https://github.com/OCA/account-invoice-reporting
+sudo git clone -b 16.0 https://github.com/OCA/account-invoice-reporting
 sudo cp -r account-invoice-reporting/account_invoice_report_due_list ../prod
-sudo git clone -b 14.0 https://github.com/OCA/account-invoicing
+sudo git clone -b 16.0 https://github.com/OCA/account-invoicing
 sudo cp -r account-invoicing/account_invoice_tax_note ../prod
 sudo cp -r account-invoicing/account_invoice_tax_required ../prod
-sudo git clone -b 14.0 https://github.com/OCA/reporting-engine
+sudo git clone -b 16.0 https://github.com/OCA/reporting-engine
 sudo cp -r reporting-engine/report_xlsx ../prod
 sudo cp -r reporting-engine/report_xlsx_helper ../prod
-sudo git clone -b 14.0 https://github.com/OCA/mis-builder
+sudo git clone -b 16.0 https://github.com/OCA/mis-builder
 sudo cp -r mis-builder/mis_builder .. /prod
 
 
 #LOCALITZACIÓ ESPANYOLA
-sudo git clone -b 14.0 https://github.com/OCA/l10n-spain
+sudo git clone -b 16.0 https://github.com/OCA/l10n-spain
 sudo cp -r l10n-spain/l10n_es_partner* ../prod
 sudo cp -r l10n-spain/l10n_es_account_* ../prod
 sudo cp -r l10n-spain/l10n_es_mis_report ../prod
@@ -30,7 +30,7 @@ sudo cp -r l10n-spain/l10n_es_aeat_mod303 ../prod
 sudo cp -r l10n-spain/l10n_es_aeat_mod390 ../prod
 
 #MÒDUL DE CONTRACTES
-sudo git clone -b 14.0 https://github.com/OCA/contract
+sudo git clone -b 16.0 https://github.com/OCA/contract
 sudo cp -r contract/contract ../prod/
 sudo cp -r contract/contract_payment_mode ../prod/
 sudo cp -r contract/agreement_legal ../prod/
@@ -38,7 +38,7 @@ sudo cp -r contract/agreement_legal ../prod/
 
 
 #TRASPAS AL DIRECTORI D'ADDONS
-sudo cp -r /home/bitnami/escanerad/addons/prod/* /bitnami/odoo/addons
+sudo cp -r /home/bitnami/escanerad/addons/prod/* opt/escaner/odoo/addons
 
 #modificar odoo-server.conf perque apunti al directori /opt/bitnami/apps/escaner/addons
 #sudo sed -i 's/addons_path/smtp_from \= "'$correu'"/g' /opt/bitnami/apps/odoo/conf/odoo-server.conf
